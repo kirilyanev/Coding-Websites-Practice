85d5e566fda801e60b05775b6f8a00066a3ced29
Kiril Yanev <kl_yanev@abv.bg>
2022-12-27 00:01:23 +0200

    Update ASME-to-SAME

--- HackerEarth/Basic Programming/ASME-to-SAME-2.py
+++ HackerEarth/Basic Programming/ASME-to-SAME-2.py
@@ -1,5 +1,4 @@
 number_of_tests = int(input())
-is_same = True
 question = '?'
 
 for test in range(0, number_of_tests):
@@ -11,25 +10,13 @@ for test in range(0, number_of_tests):
     result_a = ''.join(sorted(string_a))
     result_b = ''.join(sorted(string_b))
 
-    questions_count = 0
-
-    for i in range(0, string_length):
-        char = result_a[i]
-
-        if char != question and char not in result_b:
-            is_same = False
-        elif char == question and question in result_a:
-            questions_count += 1
-
     if question in result_a:
         index_of_question = result_a.index(question)
+        questions_count = result_a.count(question)
+        substring_a = result_a[index_of_question:questions_count]
+        result_a = result_a.replace(substring_a, '')
 
-    substring_a = result_a[int(index_of_question):questions_count]
-    substring_b = result_b[int(index_of_question):questions_count]
-    result_a = result_a.replace(substring_a, '')
-    result_b = result_b.replace(substring_b, '')
-
-    if result_a == result_b:
+    if result_a in result_b:
         print('Yes')
     else:
         print('No')
